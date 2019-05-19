cc46ba00f8a40fadde40cba89b4579256925b46f
Kevinq181 <kevin.kjqm@gmail.com>
2019-05-19 12:08:21 -0500

    Insert

--- administrator/adm_birds/adm_birds_dll/insertardatos.php
+++ administrator/adm_birds/adm_birds_dll/insertardatos.php
@@ -1,18 +1,66 @@
 <?php
-include("../../security/security.php");
 
-$con = mysqli_connect ("localhost", "root", "", "podocarpusdb");
-if (mysqli_connect_errno()) {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}else{
-    //mysqli_query($con, "SELECT * FROM aves");
-    $nombre = $_POST[nombre];
-    mysqli_query($con, "INSERT INTO aves (nombre,nombre_c,descripcion,habitat) VALUES ('$_POST[nombre]', '$_POST[nombre_cientifico]', '$_POST[descripcion]', '$_POST[habitat]')");
-    mysqli_close($con);
+include('../../adm_header.php');
 
-    echo '<script>alert("Registrado con exito");</script>';
-    echo '';
+
+$miconexion->conectar("localhost", "root", "", "podocarpusdb");
+
+if (isset($_FILES['img'])) {
+    $nombreImg = $_FILES['img']['name'];
+    $ruta = $_FILES['img']['tmp_name'];
+    $destino = "imagenes/" . $nombreImg;
+    if (copy($ruta, $destino)) {
+        //$sql="INSERT INTO `tbimg`(nombre,ruta) VALUES ('$nombreImg','$destino')";
+        $miconexion->consulta("INSERT INTO aves (nombre,nombre_c,descripcion,habitat, imagen) VALUES ('$_POST[nombre]', '$_POST[nombre_cientifico]', '$_POST[descripcion]', '$_POST[habitat]','$destino')");
+        //$res = mysqli_query($cn, mysqli_query($con, "INSERT INTO aves (nombre,nombre_c,descripcion,habitat, imagen) VALUES ('$_POST[nombre]', '$_POST[nombre_cientifico]', '$_POST[descripcion]', '$_POST[habitat]','$destino')"););
+        if ($miconexion) {
+            echo '<script type="text/javascript"> alert("Agregado Correctamente"); window.location="../adm_birds_internas/insert.php";</script>';
+        } else {
+            echo '<script type="text/javascript"> alert("Fallido"); window.location="../adm_birds_internas/insert.php";</script>';
+            die("Error" . mysqli_error($miconexion));
+        }
+    }
 }
 
+
+/*
+error_reporting(E_ALL ^ E_NOTICE);
+$miconexion->conectar("localhost", "root", "", "podocarpusdb");
+if ($miconexion->consulta("INSERT INTO aves (nombre,nombre_c,descripcion,habitat, imagen) VALUES ('$_POST[nombre]', '$_POST[nombre_cientifico]', '$_POST[descripcion]', '$_POST[habitat]',1)") == 1) {
+    echo '<script type="text/javascript"> alert("Agregado Correctamente"); window.location="../adm_birds__internas/insert.php";</script>';
+} else {
+    echo '<script>alert("Fallido");</script>';
+}
+$miconexion = new clase_mysqli();
+$miconexion->conectar("localhost", "root", "","podocarpusdb");
+
+if (mysqli_connect_errno()) {
+echo "Failed to connect to MySQL: " . mysqli_connect_error();
+} else {
+error_reporting(E_ALL ^ E_NOTICE);
+/*$foto=$_FILES["foto"]["name"];
+$ruta=$_FILES["foto"]["tmp_name"];
+$destino="fotos/".$foto;
+copy($ruta,$destino);
+$miconexion->consulta("INSERT INTO aves (nombre,nombre_c,descripcion,habitat, imagen) VALUES ('$_POST[nombre]', '$_POST[nombre_cientifico]', '$_POST[descripcion]', '$_POST[habitat]','das')");
+//mysqli_query($con, "INSERT INTO aves (nombre,nombre_c,descripcion,habitat, imagen) VALUES ('$_POST[nombre]', '$_POST[nombre_cientifico]', '$_POST[descripcion]', '$_POST[habitat]','$destino')");
+//echo '<script>
+    alert("Registrado con exito");
+</script>';
+//echo '<script>
+    location.href = "../adm_birds_internas/insert.php"
+</script>';
+require_once 'conexion.php';
+$sql= mysql_query("select * from aves");
+while($res= mysql_fetch_array($sql)){
+echo '<img src="'.$res[" imagen"].'" width="100" heigth="100"><br>';
+
+}
+//header("Location: ../adm_birds_internas/insert.php");
+//mysqli_close($miconexion);
+//echo '<script>
+    location.href = "insertardatos.php"
+</script>';
+}*/
 
 ?>
--- administrator/adm_birds/adm_birds_internas/insert.php
+++ administrator/adm_birds/adm_birds_internas/insert.php
@@ -3,46 +3,72 @@ include('../../adm_header.php');
 ?>
 <main>
 	<link href="https://fonts.googleapis.com/css?family=Baloo+Bhai" rel="stylesheet">
-	<link href='../adm_birds_css/insert.css' rel='stylesheet' type='text/css' />
-	
-	
-	<form class='form' method="post" action="../adm_birds_dll/insertardatos.php">
-		<h2>ESPECIE</h2>
-			<div class="">
-				<div class="form_item col-12 col-lg-5">
-					<input type="nombre" name="nombre" id="nombre" class="form_input" required>
-					<label for="nombre" class="form_label">NOMBRE DE ESPECIE</label>
-				</div>
-				<div class="form_item col-12 col-lg-5">
-					<input type="nombre_cientifico" name="nombre_cientifico" id="nombre_cientifico" class="form_input" required>
-					<label for="nombre_cientifico" class="form_label">NOMBRE CIENTIFICO</label>
-				</div>
+	<link href='../adm_birds_css/estiloI.css' rel='stylesheet' type='text/css' />
+
+
+	<form class='form' method="post" action="../adm_birds_dll/insertardatos.php" enctype="multipart/form-data">
+		<h2>REGISTRAR ESPECIE</h2>
+		<div class="">
+			<div class="form_item col-12 col-lg-5">
+				<input type="nombre" name="nombre" id="nombre" class="form_input" required>
+				<label for="nombre" class="form_label">NOMBRE DE ESPECIE</label>
 			</div>
 			<div class="form_item col-12 col-lg-5">
-				<input type="habitat" name="habitat" id="habitat" class="form_input" required>
-				<label for="habitat" class="form_label">HABITAT</label>
+				<input type="nombre_cientifico" name="nombre_cientifico" id="nombre_cientifico" class="form_input" required>
+				<label for="nombre_cientifico" class="form_label">NOMBRE CIENTIFICO</label>
 			</div>
+		</div>
+		<div class="form_item col-12 col-lg-5">
+			<input type="habitat" name="habitat" id="habitat" class="form_input" required>
+			<label for="habitat" class="form_label">HABITAT</label>
+		</div>
 
-			<div class="row justify-content-center">
-				<div class="form_item col-11">
-					<input class="form_input form_upload" type="file" id="file" name="file" multiple>
-					<label class="form_label">ELEGIR IMAGEN</label>
-				</div>
+		<div class="row justify-content-center">
+			<div class="form_item col-11">
+				<input class="form_input form_upload" type="file" name="img" id="foto" required>
+				<label class="form_label">ELEGIR IMAGEN</label>
 			</div>
+		</div>
 
-			<div class="row justify-content-center">
-				<div class="form_item col-11">
-					<textarea class="form_textarea" id="textarea" name="descripcion" rows="3" cols="444" required></textarea>
-					<label for="textarea" class="form_label">DESCRIPCIÓN</label>
-				</div>
+		<div class="row justify-content-center">
+			<div class="form_item col-11">
+				<textarea class="form_textarea" id="textarea" name="descripcion" rows="3" cols="444" required ></textarea>
+				<label for="textarea" class="form_label">DESCRIPCIÓN</label>
 			</div>
+		</div>
 
-			<div class="row justify-content-center">
-				<button class="submit" type="submit">REGISTRAR</button>
-			</div>
+		<div class="row justify-content-center">
+			<button class="submit" type="submit">REGISTRAR</button>
+		</div>
 	</form>
 
+	
 </main>
+
 <?php
 include('../../adm_footer.php');
 ?>
+
+<!--?php
+error_reporting(E_ALL ^ E_NOTICE);
+$miconexion->conectar("localhost", "root", "", "podocarpusdb");
+$foto = $_FILES["imagen"]["name"];
+$ruta = $_FILES["imagen"]["tmp_name"];
+$destino = "imagenes/" . $foto;
+copy($ruta, $destino);
+
+if($miconexion->consulta("INSERT INTO aves (nombre,nombre_c,descripcion,habitat, imagen) VALUES ('$_POST[nombre]', '$_POST[nombre_cientifico]', '$_POST[descripcion]', '$_POST[habitat]','$destino')")==1){
+	include('../adm_birds_dll/insertardatos.php');
+}else{
+	echo '<script>location.href="../adm_birds_internas/insert.php"</script>';
+}
+
+
+
+//mysqli_query($con, "INSERT INTO aves (nombre,nombre_c,descripcion,habitat, imagen) VALUES ('$_POST[nombre]', '$_POST[nombre_cientifico]', '$_POST[descripcion]', '$_POST[habitat]','$destino')");
+//echo '<script>alert("Registrado con exito");</script>';
+//header("Location: ../adm_birds_internas/insert.php");
+//echo '<script>location.href="../adm_birds_internas/insert.php"</script>';
+//mysqli_close($miconexion);
+?>
+
